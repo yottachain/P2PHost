484a25bf22ba81ff47f5d324f1af910c1e7faf3a
zhangzhengyan <zzy_19831106@163.com>
2020-05-15 20:49:26 +0800

    ythost modify

--- server.go
+++ server.go
@@ -89,15 +89,16 @@ func (server *Server) Connect(ctx context.Context, req *pb.ConnectReq) (*pb.Empt
 
 // DisConnect implemented DisConnect function of P2PHostServer
 func (server *Server) DisConnect(ctx context.Context, req *pb.StringMsg) (*pb.Empty, error) {
-	ID, err := peer.Decode(req.GetValue())
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, err.Error())
-	}
-	
-	err = server.Host.ClientStore().Close(ID)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, err.Error())
-	}
+	server.CliPool.Close(req.GetValue())
+	//ID, err := peer.Decode(req.GetValue())
+	//if err != nil {
+	//	return nil, status.Errorf(codes.Internal, err.Error())
+	//}
+	//
+	//err = server.Host.ClientStore().Close(ID)
+	//if err != nil {
+	//	return nil, status.Errorf(codes.Internal, err.Error())
+	//}
 	return &pb.Empty{}, nil
 }
 
